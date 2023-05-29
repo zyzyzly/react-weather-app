90ab00bdf2d4e69b5a863602f5288902e590d41e
zyzyzly <zeinabzeinali71@gmail.com>
2023-05-29 19:13:28 +0330

    added html to weather component

---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city.."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="Mostly Cloudy"
+          />
+          6℃
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15% </li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
