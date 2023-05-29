ffd0b216a069db37d7758565980e9d09604fadd7
zyzyzly <zeinabzeinali71@gmail.com>
2023-05-29 18:33:53 +0330

    added the bootstrap

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/zeinab-zeinali-164400260"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Zeinab Zeinali
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/wecodeschool/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/zeinab-zeinali-164400260"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Zeinab Zeinali
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/wecodeschool/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
