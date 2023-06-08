383c1c03622ef8cfb18e430006a76907101310cc
zyzyzly <zeinabzeinali71@gmail.com>
2023-06-08 19:35:03 +0330

    fixed error

--- src/Weather.js
+++ src/Weather.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
 
 import axios from "axios";
-import "./Weather.css";
+import "./weather.css";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
